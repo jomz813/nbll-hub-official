0632f4798a5eb97253f0f004f778264fc0ed95c7
jomz813 <spidermonkeyy813@gmail.com>
2026-01-22 23:06:43 -0500

    Update schedule.ts

--- data/schedule.ts
+++ data/schedule.ts
@@ -1,31 +1,14 @@
-
 export const scheduleData = [
   {
     week: 6,
     games: [
       {
         title: 'Game 1',
-        matchups: [
-          'Dallas Mavericks @ Orlando Magic',
-          'Los Angeles Clippers @ Boston Celtics',
-          'Los Angeles Lakers @ Miami Heat',
-          'Toronto Raptors @ Denver Nuggets',
-          'Minnesota Timberwolves @ Memphis Grizzlies',
-          'Chicago Bulls @ Milwaukee Bucks',
-          'Atlanta Hawks @ Oklahoma City Thunder'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       },
       {
         title: 'Game 2',
-        matchups: [
-          'Orlando Magic @ Chicago Bulls',
-          'Miami Heat @ Atlanta Hawks',
-          'Memphis Grizzlies @ Los Angeles Clippers',
-          'Boston Celtics @ Toronto Raptors',
-          'Oklahoma City Thunder @ Dallas Mavericks',
-          'Milwaukee Bucks @ Los Angeles Lakers',
-          'Denver Nuggets @ Minnesota Timberwolves'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       }
     ]
   },
@@ -34,27 +17,11 @@ export const scheduleData = [
     games: [
       {
         title: 'Game 1',
-        matchups: [
-          'Milwaukee Bucks @ Dallas Mavericks',
-          'Los Angeles Clippers @ Los Angeles Lakers',
-          'Orlando Magic @ Miami Heat',
-          'Memphis Grizzlies @ Oklahoma City Thunder',
-          'Chicago Bulls @ Denver Nuggets',
-          'Atlanta Hawks @ Toronto Raptors',
-          'Boston Celtics @ Minnesota Timberwolves'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       },
       {
         title: 'Game 2',
-        matchups: [
-          'Denver Nuggets @ Milwaukee Bucks',
-          'Dallas Mavericks @ Atlanta Hawks',
-          'Los Angeles Lakers @ Orlando Magic',
-          'Miami Heat @ Memphis Grizzlies',
-          'Toronto Raptors @ Chicago Bulls',
-          'Oklahoma City Thunder @ Boston Celtics',
-          'Minnesota Timberwolves @ Los Angeles Clippers'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       }
     ]
   },
@@ -63,27 +30,11 @@ export const scheduleData = [
     games: [
       {
         title: 'Game 1',
-        matchups: [
-          'Dallas Mavericks @ Los Angeles Lakers',
-          'Minnesota Timberwolves @ Chicago Bulls',
-          'Los Angeles Clippers @ Miami Heat',
-          'Boston Celtics @ Milwaukee Bucks',
-          'Oklahoma City Thunder @ Toronto Raptors',
-          'Memphis Grizzlies @ Atlanta Hawks',
-          'Denver Nuggets @ Orlando Magic'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       },
       {
         title: 'Game 2',
-        matchups: [
-          'Orlando Magic @ Oklahoma City Thunder',
-          'Milwaukee Bucks @ Memphis Grizzlies',
-          'Miami Heat @ Minnesota Timberwolves',
-          'Toronto Raptors @ Los Angeles Clippers',
-          'Los Angeles Lakers @ Denver Nuggets',
-          'Atlanta Hawks @ Boston Celtics',
-          'Chicago Bulls @ Dallas Mavericks'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       }
     ]
   },
@@ -92,27 +43,11 @@ export const scheduleData = [
     games: [
       {
         title: 'Game 1',
-        matchups: [
-          'Los Angeles Lakers @ Boston Celtics',
-          'Memphis Grizzlies @ Orlando Magic',
-          'Denver Nuggets @ Chicago Bulls',
-          'Toronto Raptors @ Miami Heat',
-          'Dallas Mavericks @ Minnesota Timberwolves',
-          'Atlanta Hawks @ Milwaukee Bucks',
-          'Los Angeles Clippers @ Oklahoma City Thunder'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       },
       {
         title: 'Game 2',
-        matchups: [
-          'Chicago Bulls @ Los Angeles Lakers',
-          'Orlando Magic @ Atlanta Hawks',
-          'Oklahoma City Thunder @ Denver Nuggets',
-          'Milwaukee Bucks @ Toronto Raptors',
-          'Boston Celtics @ Dallas Mavericks',
-          'Miami Heat @ Memphis Grizzlies',
-          'Minnesota Timberwolves @ Los Angeles Clippers'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       }
     ]
   },
@@ -121,27 +56,11 @@ export const scheduleData = [
     games: [
       {
         title: 'Game 1',
-        matchups: [
-          'Minnesota Timberwolves @ Boston Celtics',
-          'Toronto Raptors @ Orlando Magic',
-          'Los Angeles Clippers @ Chicago Bulls',
-          'Denver Nuggets @ Miami Heat',
-          'Milwaukee Bucks @ Oklahoma City Thunder',
-          'Atlanta Hawks @ Dallas Mavericks',
-          'Los Angeles Lakers @ Memphis Grizzlies'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       },
       {
         title: 'Game 2',
-        matchups: [
-          'Boston Celtics @ Denver Nuggets',
-          'Orlando Magic @ Minnesota Timberwolves',
-          'Chicago Bulls @ Atlanta Hawks',
-          'Memphis Grizzlies @ Toronto Raptors',
-          'Oklahoma City Thunder @ Los Angeles Lakers',
-          'Dallas Mavericks @ Los Angeles Clippers',
-          'Miami Heat @ Milwaukee Bucks'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       }
     ]
   },
@@ -150,27 +69,11 @@ export const scheduleData = [
     games: [
       {
         title: 'Game 1',
-        matchups: [
-          'Orlando Magic @ Boston Celtics',
-          'Miami Heat @ Chicago Bulls',
-          'Oklahoma City Thunder @ Denver Nuggets',
-          'Memphis Grizzlies @ Dallas Mavericks',
-          'Los Angeles Clippers @ Milwaukee Bucks',
-          'Toronto Raptors @ Los Angeles Lakers',
-          'Atlanta Hawks @ Minnesota Timberwolves'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       },
       {
         title: 'Game 2',
-        matchups: [
-          'Dallas Mavericks @ Denver Nuggets',
-          'Chicago Bulls @ Memphis Grizzlies',
-          'Milwaukee Bucks @ Orlando Magic',
-          'Boston Celtics @ Miami Heat',
-          'Minnesota Timberwolves @ Los Angeles Clippers',
-          'Oklahoma City Thunder @ Toronto Raptors',
-          'Los Angeles Lakers @ Atlanta Hawks'
-        ]
+        matchups: [' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ', ' @ ']
       }
     ]
   }
